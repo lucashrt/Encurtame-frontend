c89ca685d57872792d5c5c3ee1c438161bedc863
lucashrt <hartmann.lucas@hotmail.com>
2025-06-09 13:11:25 -0300

    Refactor result display and copy functionality in URL shortening service

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
-function shorten() {
-    const input = document.querySelector('#inputUrl').value;
-    const output = document.querySelector('div#result');
+const urlPost = 'http://localhost:3000/shorten';
 
-    //conectar com o backend
+async function shorten() {
+    const input = document.querySelector('#inputUrl');
+    const output = document.querySelector('div#result');
+    const shortUrl = document.querySelector('div#shortUrl');
+
+    const originalUrl = input.value;
+
+    try {
+        const response = await axios.post(`${urlPost}`, {
+            originalUrl: originalUrl
+        });
+
+        output.style.display = 'block';
+        shortUrl.innerHTML = `<input type="text" class="text" value="${response.data.shortUrl}" readonly><button id="copyButton"><i class="fa fa-clone"></i></button>`;
+        copyText();
+    } catch (error) {
+        console.error('Erro ao encurtar a URL:', error);
+        output.innerHTML = '<p>Erro ao encurtar a URL. Tente novamente.</p>';
+    }
+}
+
+function copyText() {
+    const copyText = document.querySelector(".copy-text");
+    const button = copyText.querySelector("button");
+    const input = copyText.querySelector("input.text");
+
+    button.addEventListener("click", () => {
+        navigator.clipboard.writeText(input.value)
+            .then(() => {
+                copyText.classList.add("active");
+
+                setTimeout(() => {
+                    copyText.classList.remove("active");
+                }, 1500);
+            })
+            .catch(err => {
+                console.error("Erro ao copiar o texto: ", err);
+            });
+    });
 }
