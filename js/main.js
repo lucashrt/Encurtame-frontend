be44c04ab5662357d924df5468eff34e6deb12f5
lucashrt <hartmann.lucas@hotmail.com>
2025-06-10 13:23:28 -0300

    Add error handling for invalid URLs and update button functionality in URL shortening service

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ async function shorten() {
     const input = document.querySelector('input#inputUrl');
     const output = document.querySelector('div#result');
     const shortUrl = document.querySelector('div#shortUrl');
+    const label = document.getElementsByClassName('label')[0];
+    const error = document.getElementsByClassName('error')[0];
 
     const originalUrl = input.value;
 
@@ -12,8 +14,11 @@ async function shorten() {
             originalUrl: originalUrl
         });
 
-        output.style.display = 'block';
+        error.style.display = 'none';
         shortUrl.innerHTML = `<input type="text" class="text" value="${response.data.shortUrl}" readonly><button id="copyButton"><i class="fa fa-clone"></i></button>`;
+        label.style.display = 'block';
+        output.style.display = 'block';
+        shortUrl.style.display = 'block';
         copyText();
     } catch (error) {
         console.error('Erro ao encurtar a URL:', error);
@@ -39,4 +44,25 @@ function copyText() {
                 console.error("Erro ao copiar o texto: ", err);
             });
     });
+}
+
+function startBtn() {
+    const input = document.querySelector('input#inputUrl').value;
+    const output = document.querySelector('div#result');
+    const shortUrl = document.querySelector('div#shortUrl');
+    const label = document.getElementsByClassName('label')[0];
+    const error = document.getElementsByClassName('error')[0];
+
+    const regex = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\S*)?$/;
+
+    if (regex.test(input)) {
+        shorten();
+    } else {
+        shortUrl.style.display = 'none';
+        label.style.display = 'none';
+        output.style.display = 'block';
+        error.style.display = 'block';
+        error.innerHTML = '<p>URL inválida!</p>';
+        error.innerHTML += '<p>Tente novamente</p>';
+    }
 }
